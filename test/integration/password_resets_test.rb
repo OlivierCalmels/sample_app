e6bebd47b917ad16b2eed700cb8dd7eca96dc635
OlivierCalmels <calmels.o@gmail.com>
2021-02-15 11:29:51 +0100

    Add password reset

--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -60,7 +60,7 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
                             password_confirmation:  "" } }
     assert_select 'div#error_explanation'
 
-    # Empty password
+    # Valid password & confirmation
     patch password_reset_path(user.reset_token),
           params: { email: user.email,
                     user: { password:               "foobaz",
@@ -69,4 +69,23 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     assert_redirected_to user
   end
+
+  test "expired token" do
+  
+    get new_password_reset_path
+    post  password_resets_path,
+          params: { password_reset: { email: @user.email } }
+
+    @user = assigns(:user)
+    @user.update_attribute(:reset_sent_at, 3.hours.ago)
+    patch password_reset_path(@user.reset_token),
+          params: { email: @user.email,
+                    user: { password:               "foobar",
+                            password_confirmation:  "foobar" } }
+    assert_response :redirect
+    follow_redirect!
+    assert_match /expired/i, response.body
+
+  end
+
 end
