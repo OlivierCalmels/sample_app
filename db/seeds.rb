038789bf97bead201da0b137945937ce83af1965
OlivierCalmels <calmels.o@gmail.com>
2021-02-16 12:47:19 +0100

    13.2 showing microposts done

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,4 +27,15 @@ User.create!( name: "Example User",
                 activated: true,
                 activated_at: Time.zone.now)
   p "#{n} - #{name} created!"
+
+
+# Generate microposts for a subset of users
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(word_count: 5)
+  users.each { |user| user.microposts.create!(content: content) }
+end
+
+
+
 end
