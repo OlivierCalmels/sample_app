6c7c7be2e0e2a4b48f9e77bda5d522177d633015
OlivierCalmels <calmels.o@gmail.com>
2021-02-26 15:59:37 +0100

    resolving conflicts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,7 @@ Rails.application.configure do
    config.action_mailer.delivery_method = :smtp
    host = 'nameless-waters-84271.herokuapp.com'
    config.action_mailer.default_url_options = { host: host}
+
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
